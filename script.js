515f15a47907ed2e0869d188b96c45db7d2f37cd
kesava <kesavadananjaya@gmail.com>
2023-07-11 22:30:24 +0800

    hide the menu when user submit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const playerData = document.getElementById('player-data');
+const menu = document.getElementById('menu');
+
 
 playerData.addEventListener('submit',evt => {
     evt.preventDefault();
     const data = Object.fromEntries(new FormData(playerData));
+    menu.classList.add('hide');
 });
